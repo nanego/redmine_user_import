3ff5154ba9da0341a53ae221b86fc7da6ca811c3
mathieu-mbru <m.brulin@adacis.net>
2020-12-15 14:18:37 +0100

    Mise à jour le test apres avoir désactivé : ""Créer les valeurs manquantes de l'organisation""

--- spec/models/user_import_spec.rb
+++ spec/models/user_import_spec.rb
@@ -23,7 +23,7 @@ def generate_import_with_mapping(fixture_name='import_users.csv')
 
    import.settings = {
      'separator' => ";", 'wrapper' => '"', 'encoding' => "UTF-8", "notifications"=>"0",
-     'mapping' => {"login"=>"0", "firstname"=>"2", "lastname"=>"1", "mail"=>"3", "organization"=>"4", "create_organizations"=>"1"},
+     'mapping' => {"login"=>"0", "firstname"=>"2", "lastname"=>"1", "mail"=>"3", "organization"=>"4", "create_organizations"=>"0"},
    }
    import.save!
    import
@@ -32,29 +32,27 @@ end
 RSpec.describe UserImport, type: :model do
     fixtures :users, :email_addresses,:members , :member_roles, :roles, :projects, :organizations
 
-    it "should_create_missing_organization" do
-        organization_count_before = Organization.count
-    import = generate_import_with_mapping
-    import.save!
-    import.run
-    # test add new Organization
-    organization_count_after = Organization.count
-    expect(organization_count_after).to eq(organization_count_before + 2)
-
+    it "should_update_organization_of_users" do   
+      import = generate_import_with_mapping
+      import.save!
+      import.run
+      # test update Organization
+      expect(User.all[-2].organization_id).to eq (1) 
+      expect(User.last.organization_id).to eq (nil) 
     end
 
     it "should_not_import_user_already_existed" do
-        user_count_before = User.count
-    import = generate_import_with_mapping('import_users_exists.csv')
-    import.save!
-    import.run
-    user_count_after = User.count
+      user_count_before = User.count
+      import = generate_import_with_mapping('import_users_exists.csv')
+      import.save!
+      import.run
+      user_count_after = User.count
 
-    expect(ImportItem.first.message).to eq("Email has already been taken\nLogin has already been taken")
+      expect(ImportItem.first.message).to eq("Email has already been taken\nLogin has already been taken")
 
-    expect(user_count_before).to eq(user_count_after - 1)
-    # update user's organization
-    expect(User.find(3).organization_id).to eq(Organization.fourth.id)
+      expect(user_count_before).to eq(user_count_after - 1)
+      # update user's organization
+      expect(User.find(3).organization_id).to eq(1)
 
     end
 
