a13a65455b1147c4f34d11f0fb639911ed77d606
Vincent <contact@vincent-robert.com>
2025-06-11 09:00:45 +0200

    Improve Redmine 6 compatibility: simplify overrides paths

--- app/overrides/users/index.rb
+++ app/overrides/users/index.rb
@@ -1,4 +1,4 @@
 Deface::Override.new :virtual_path  => "users/index",
                      :name          => "add-link-to-user-import",
-                     :insert_before => "erb[loud]:contains('link_to l(:label_user_new)')",
+                     :insert_before => "erb[loud]:contains('l(:label_user_new)')",
                      :text          => "<%= link_to l(:label_import_users_csv), new_user_imports_path, :class=>'icon icon-import' if User.current.admin? %>"
