a92c90cb56bed2a773cf1011b8026c380de31ed3
Vincent <contact@vincent-robert.com>
2026-07-22 08:46:18 +0200

    Migrate legacy icon classes to SVG sprite_icon for Redmine 7.0

--- app/overrides/projects/index.rb
+++ app/overrides/projects/index.rb
@@ -1,4 +1,4 @@
 Deface::Override.new :virtual_path  => "projects/index",
                      :name          => "add-link-to-user-import",
                      :insert_bottom => "div.contextual",
-                     :text          => "<%= link_to l(:label_import_users_csv), new_user_imports_path, :class=>'icon icon-import' if User.current.allowed_to?(:users_import, nil, :global => true) %>"
+                     :text          => "<%= link_to sprite_icon('import', l(:label_import_users_csv)), new_user_imports_path if User.current.allowed_to?(:users_import, nil, :global => true) %>"
